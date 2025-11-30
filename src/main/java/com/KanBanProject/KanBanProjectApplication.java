e41f6677f0a5ae3a993ecaef5901a0f4321b9cb9
Vettas Alexandros <alexandros.vettas@gmail.com>
2025-11-30 15:00:30 +0200

    Add startup initializer for default board, lists, and task
    Introduces a CommandLineRunner bean that creates a default board, three lists (To Do, In Progress, Done), and a sample task on application startup. This provides a starter pack for new deployments to facilitate initial testing and usage.

--- src/main/java/com/KanBanProject/KanBanProjectApplication.java
+++ src/main/java/com/KanBanProject/KanBanProjectApplication.java
@@ -1,13 +1,62 @@
 package com.KanBanProject;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.KanBanProject.entity.Board;
+import com.KanBanProject.entity.ListEntity;
+import com.KanBanProject.entity.Task;
+import com.KanBanProject.service.BoardService;
+import com.KanBanProject.service.ListService;
+import com.KanBanProject.service.TaskService;
 
 @SpringBootApplication
 public class KanBanProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KanBanProjectApplication.class, args);
+	}
+
+	//Create my default StarterPack (one Board, 3 Lists and one task) on-run.
+	@Bean
+	CommandLineRunner initDefaultStarterPack(BoardService boardService, ListService listService, TaskService taskservice) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... Args) {
+				System.out.println("StartUp init running..");
+
+				Board defaultBoard = new Board();
+				defaultBoard.setName("Default Board");
+
+				Board savedBoard = boardService.createBoard(defaultBoard);
+				Long boardId = savedBoard.getId();
+			
+
+
+				ListEntity toDo = new ListEntity();
+				toDo.setName("To Do");
+				listService.createList(boardId, toDo);
+				ListEntity savedToDo = listService.createList(boardId, toDo);
+
+
+				ListEntity inProgress = new ListEntity();
+				inProgress.setName("In Progress");
+				listService.createList(boardId, inProgress);
+				ListEntity savedInProgress = listService.createList(boardId, inProgress);
+
+
+				ListEntity done = new ListEntity();
+				done.setName("Done");
+				listService.createList(boardId, done);
+				ListEntity savedDone = listService.createList(boardId, done);
+
+				Task task = new Task();
+				task.setName("My Task");
+				taskservice.createTask(savedToDo.getId(), task);
+			}
+		};
 	}
 
 }
